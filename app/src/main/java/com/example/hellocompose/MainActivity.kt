07c3ba2866b1a6da293ce9f27cfd03cafb8f1b9a
Edorta Rodriguez <edorta99@hotmail.com>
2021-12-22 16:46:16 +0100

    Making Card

--- app/src/main/java/com/example/hellocompose/MainActivity.kt
+++ app/src/main/java/com/example/hellocompose/MainActivity.kt
@@ -5,13 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.hellocompose.ui.theme.HelloComposeTheme
 
 
@@ -19,25 +23,35 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-          HelloCompose()
+            HelloCompose()
         }
     }
 }
 
 @Composable
-fun HelloCompose(){
-    Column {
-        Image(painterResource(id = R.drawable.platzi), contentDescription = null)
-        Text("Este curso es lo mejor")
-        Text("Hello Compose")
-        Button(onClick = {  }) {
-            Text(text = "Click me")
+fun HelloCompose() {
+    Card(elevation = 4.dp, shape = RoundedCornerShape(20.dp)) {
+        Column(
+            modifier = Modifier.padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painterResource(id = R.drawable.platzi),
+                contentDescription = null,
+                modifier = Modifier.size(40.dp, 40.dp)
+            )
+            Text("Este curso es lo mejor", style = MaterialTheme.typography.h4)
+            Text("Hello Compose")
+            Button(onClick = { }, modifier = Modifier.padding(top = 16.dp)) {
+                Text(text = "Click me")
+            }
         }
     }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun HelloComposePreview(){
+fun HelloComposePreview() {
     HelloCompose()
 }
